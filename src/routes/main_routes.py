20e4f0d18f39c7fc2a7d2b1b7184ea1b92f83a21
Gabriel <gabaraujoteixeira@gmail.com>
2023-11-20 22:31:19 -0300

    crud dependentes

--- src/routes/main_routes.py
+++ src/routes/main_routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 from routes.agencia import agenciaRouter
 from routes.funcionario import funcionarioRouter
-
+from routes.dependente import dependenteRouter
 mainRoutes = APIRouter()
 
 mainRoutes.include_router(agenciaRouter, prefix="/agencia", tags=["agencia"])
 mainRoutes.include_router(funcionarioRouter, prefix="/funcionario", tags=["funcionario"])
+mainRoutes.include_router(dependenteRouter, prefix="/dependente", tags=["dependente"])
