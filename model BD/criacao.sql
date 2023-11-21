8b1a79a510ddbf705b28dc3e317d50995c55eb20
inaciolimaf <92096585+inaciolimaf@users.noreply.github.com>
2023-11-21 20:02:37 -0300

    Colocando em arquivos separedos de sql

--- model BD/criacao.sql
+++ model BD/criacao.sql
@@ -430,7 +430,3 @@ DELIMITER ;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-INSERT INTO `nullbank`.`Endereco` (`idEndereco`, `tipo_logradouro`, `nome_logradouro`, `numero`, `bairro`, `cep`, `cidade`, `estado`) VALUES (1, 'Rua', 'Rua Principal', 123, 'Centro', '12345-678', 'CidadeB', 'UF');
-INSERT INTO `nullbank`.`Cliente` (`cpf`, `nome_completo`, `rg`, `orgao_emissor`, `uf_rg`, `data_nascimento_cliente`, `Endereco_idEndereco`) VALUES ('12345678901', 'Cliente A', '123456789', 'SSP', 'UF', '1980-01-01', 1);
-INSERT INTO `nullbank`.`Agencia` (`idAgencia`, `nome`, `salario_montante_total`, `cidade`) VALUES (1, 'Agencia A', 0.00, 'CidadeA');
