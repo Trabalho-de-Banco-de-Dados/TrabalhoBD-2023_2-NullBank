730864e76baa6383d4edecfc9582e450eb653a1b
Wendel <tybnkdbj35@gmail.com>
2023-11-28 16:33:18 -0300

    inserido select box

--- public/login.js
+++ public/login.js
@@ -47,3 +47,42 @@ function guardarToken(data) {
 function abrirDBA() {
   window.location.href = "./DBA_page.html"
 }
+
+botaoDBA.addEventListener("click", async function (e) {
+  const input = document.getElementsByClassName("textarea").value
+  console.log(input)
+  const url = "http://127.0.0.1:8000/dba/"
+  const dadosJson = {
+    sql: input,
+  }
+
+  const token =
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvX2lkIjoiQURNSU4iLCJ0aXBvX3VzdWFyaW8iOiJEQkEiLCJleHAiOjE3MDM3MjE1MzR9.5L11PS2w4-2bqbh9nUb9pAeTEV2upqVtEtYwuoPxEHM"
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token, // Adicionando o token ao cabeçalho
+      },
+      body: JSON.stringify(dadosJson),
+    })
+
+    if (!response.ok) {
+      throw new Error(
+        `Erro na requisição: ${response.status} - ${response.statusText}`
+      )
+    }
+
+    const data = await response.json()
+    console.log("Resposta:", data)
+
+    guardarToken(data)
+    abrirDBA()
+
+    // Se necessário, faça algo com a resposta
+  } catch (error) {
+    console.error("Erro:", error)
+  }
+})
