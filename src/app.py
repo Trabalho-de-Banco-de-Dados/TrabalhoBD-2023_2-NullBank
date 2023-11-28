61c3405485a65ed9d76dca1b56295dac3993b4aa
inaciolimaf <92096585+inaciolimaf@users.noreply.github.com>
2023-11-28 17:09:15 -0300

    Ativando CORS

--- src/app.py
+++ src/app.py
@@ -16,6 +16,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(mainRoutes)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Ou você pode especificar as origens permitidas
+    allow_credentials=True,
+    allow_methods=["*"],  # Ou você pode especificar métodos permitidos (por exemplo, ["GET", "POST"])
+    allow_headers=["*"],  # Ou você pode especificar cabeçalhos permitidos
+)
 
 if __name__ == '__main__':
     uvicorn.run(app=app, host='127.0.0.1', port=8000)
