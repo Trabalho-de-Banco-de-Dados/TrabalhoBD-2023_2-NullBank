c118afbe0e4035165a4f0aabb745414adc682ad5
Gabriel <gabaraujoteixeira@gmail.com>
2023-11-27 22:08:39 -0300

    ajeitando requisção

--- src/app.py
+++ src/app.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 import uvicorn
 from routes.main_routes import mainRoutes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Nullbank",
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(mainRoutes)
 
 if __name__ == '__main__':
